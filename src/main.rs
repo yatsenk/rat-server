d80970f7d0d410405ce864667d4dccf8289537ff
Dmytro Yatsenko <dima0102yacenko@gmail.com>
2026-07-12 14:00:23 +0200

    manage client computer by command line

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,24 @@
-use std::io::prelude::*;
+use std::io::{self, prelude::*};
 use std::net::{TcpListener, TcpStream};
 
 fn handle_connection(mut stream: TcpStream) {
-    stream.read(&mut [0; 128]).unwrap();
-    println!("{:?}", stream);
+    println!("[*] client is connected");
+    loop {
+        print!(">> ");
+        io::stdout().flush().unwrap(); 
+        let mut input = String::new();
+
+        io::stdin()
+            .read_line(&mut input)
+            .expect("could not read line");
+
+        stream.write_all(input.as_bytes()).unwrap();
+    }
 }
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    println!("[*] listener is created, waiting for client ...");
 
     for stream in listener.incoming() {
         match stream {
